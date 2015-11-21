f2fd7a329c22369c9d5e9b5c8b8e25543c8c39ed
Gelidus <gelidus@flowup.eu>
2015-11-21 23:32:04 +0100

    [PacketProcessing] Added first output for testing

--- InterfaceInfoContainer.cpp
+++ InterfaceInfoContainer.cpp
@@ -14,15 +14,40 @@ InterfaceInfoContainer::~InterfaceInfoContainer() {
 }
 
 Error InterfaceInfoContainer::ProcessPacket(SNMPGetPacket *packet) {
-	cout << "Got packet id: " << packet->pdu().request_id().value() << endl;
+	// checking for valid oid
+	list<Byte> oid = packet->pdu().varbinds().binds().begin()->identifier().value();
 
-	if (packet->pdu().varbinds().binds().begin()->identifier().value().size() > 11) {
+	// if the identifier is not the actual table size (11), just return, we are
+	// out of interface objects
+	if (oid.size() != 11) {
 		return Error::SNMPNotAnInterface;
+	}
+
+	// check if the given oid is entry or not
+	auto it = oid.begin();
+	for (int i = 0; i < kSNMPEntryLength; i++, it++) {
+		if (kSNMPEntry[i] != (*it)) {
+			return Error::SNMPNotAnInterface;
+		}
+	}
+
+	// retrieve the interface from the oid
+	Byte interface = oid.back();
+
+	// check if the interface already exists
+	if (interfaces_.count(interface) < 0) {
+		// non existing interface
+		interfaces_[interface] << "time";
+	} else {
+		// existing interface
+		interfaces_[interface] << ";thing";
 	}
 
 	return Error::None;
 }
 
 void InterfaceInfoContainer::OutputResults() {
-
+	for (const auto &pair : interfaces_) {
+		cout << pair.second.str() << endl;
+	}
 }
--- InterfaceInfoContainer.h
+++ InterfaceInfoContainer.h
@@ -6,8 +6,16 @@
 #define ISA_INTERFACEINFOCONTAINER_H
 
 #include "SNMPPacket.h"
+#include <map>
+#include <sstream>
+
+const int kSNMPEntryLength = 9;
+const int kSNMPEntry[kSNMPEntryLength] = {1,3,6,1,2,1,2,2,1};
+const int kSNMPEntryInterfaceIndex = 10;
 
 class InterfaceInfoContainer {
+private:
+		std::map<Byte, std::stringstream> interfaces_;
 
 public:
 		InterfaceInfoContainer();
